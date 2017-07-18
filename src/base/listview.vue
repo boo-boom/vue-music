203aab33c40811f432c29de049a67400d9362d72
boo-boom <510126766@qq.com>
2017-07-18 17:35:10 +0800

    u

--- src/base/listview.vue
+++ src/base/listview.vue
@@ -11,11 +11,20 @@
                 </ul>
             </li>
         </ul>
+        <div class="list-shortcut">
+            <ul>
+                <li class="item" v-for="(item, index) in shortcutList" :key="index" :class="{'current':currentIndex==index}">{{item}}</li>
+            </ul>
+        </div>
+        <div class="loading-container" v-show="!data.length">
+            <loading></loading>
+        </div>
     </scroll>
 </template>
 
 <script>
 import Scroll from 'base/scroll';
+import Loading from 'base/loading';
 
 export default {
     name: 'listview',
@@ -25,9 +34,21 @@ export default {
             default: []
         }
     },
-    components: { Scroll },
+    data(){
+        return {
+            currentIndex: 0
+        }
+    },
+    components: { Scroll, Loading },
     methods: {
 
+    },
+    computed: {
+        shortcutList(){
+            return this.data.map((group) => {
+                return group.title.substr(0, 1);
+            });
+        }
     }
 }
 </script>
@@ -73,6 +94,35 @@ export default {
                 font-size: @font-size-medium;
             }
         }
+    }
+    .list-shortcut {
+        position: absolute;
+        right: 0;
+        top: 50%;
+        z-index: 30;
+        transform: translateY(-50%);
+        width: 2rem;
+        padding: 2rem 0;
+        border-radius: 1rem;
+        text-align: center;
+        background: @color-background-d;
+        font-family: Helvetica;
+        .item {
+            //font-size: 20rem;
+            padding: 0.3rem;
+            line-height: 1;
+            color: @color-text-l;
+            font-size: @font-size-small;
+            &.current{
+                color: @color-theme;
+            }
+        }
+    }
+    .loading-container {
+        position: absolute;
+        width: 100%;
+        top: 50%;
+        transform: translateY(-50%);
     }
 }
 </style>
